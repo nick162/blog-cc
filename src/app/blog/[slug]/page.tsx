c46313d8fddbda0410fb6371bba1976ef93c2845
nick162 <azwar.hanik@gmail.com>
2025-03-10 22:48:47 +0700

    blog-cc

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,23 @@ import { Suspense } from "react";
 import BlogBody from "./_components/BlogBody";
 import { BlogHeader } from "./_components/BlogHeader";
 import Loading from "@/components/Loading";
+import { getBlog } from "../_api/getBlog";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const slug = (await params).slug;
+  const blog = await getBlog(slug);
+  return {
+    title: blog[0].title,
+    description: blog[0].description,
+    openGraph: {
+      images: blog[0].image,
+    },
+  };
+};
 
 const BlogDetail = async ({
   params,
